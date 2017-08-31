f0e2dc647557f0bd898de9e01c3134c859cf44a1
Daniel Gullberg <daniel_gullberg@hotmail.com>
2017-08-31 20:19:53 +0200

    Atmel Studio 7 project

---
+++ throttle_controller/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2017-08-31 19:44:25
+ * Author : Daniel
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ throttle_controller/throttle_controller/Sketch.cpp
@@ -0,0 +1,135 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+#include "motor.hpp"
+#include "pid.hpp"
+//Beginning of Auto generated function prototypes by Atmel Studio
+void wInterrupt();
+//End of Auto generated function prototypes by Atmel Studio
+
+
+
+#define MOTOR_PWM 9
+#define MOTOR_A1  10
+#define MOTOR_A2  11
+#define MOTOR_POS (unsigned char)1
+#define REF_IN    0
+#define W_INTERRUPT 0
+
+Motor motor(MOTOR_PWM, MOTOR_A1, MOTOR_A2);
+
+volatile uint32_t wCounter;
+
+PID pid;
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+  motor.init();
+  pid.init();
+  attachInterrupt(W_INTERRUPT,wInterrupt, RISING);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  int pos;
+  static int ref = 500;
+  int refIn;  //!< Analog reference value input
+  int u;
+  int t;
+  static unsigned int ct;
+  static int tOld;
+  static int refList[] = {100,200,300,200,100,800,100,800,700,690,680,670,660,650,645,640,635};
+  int stepTime=500;
+  uint32_t wCounterTemp;
+  static uint32_t wCounterTempOld;
+  int nEng;
+  /*
+  int pause = 300;
+  motor.forward(speed);
+  delay(pause);
+  motor.forward(0);
+  delay(pause);
+  motor.reverse(speed);
+  delay(pause);
+  motor.stop();
+  delay(pause);*/
+  pos = analogRead(MOTOR_POS);
+  refIn = analogRead(REF_IN);
+
+  ref = refIn-200;
+  ref = max(200,ref);
+  ref = min(800,ref);
+  if(pid.calculate((double)ref, (double)pos))
+  {
+    u=-(int)pid.getOutput();
+    // Stop motor if the output is small enough
+    if(abs(u)<40)
+    {
+      motor.stop();
+    }
+    else
+    {
+      motor.speed(u);
+    }
+
+    noInterrupts();
+    wCounterTemp = wCounter;
+    interrupts();
+
+    nEng=wCounterTemp-wCounterTempOld;
+    wCounterTempOld=wCounterTemp;
+
+    Serial.println(nEng);
+//    Serial.print(" ");
+//    Serial.print(ref);
+//    Serial.print(" ");
+//    Serial.println(refIn);
+  }
+  
+  if(Serial.available() > 0)
+  {
+    char chr;
+    chr = Serial.read();
+    Serial.print("Read ");
+    Serial.println(chr);
+    switch(chr)
+    {
+      case 'R':
+        // Reset
+       // wdt_enable(WDTO_15MS);
+       // while(1) {};
+        break;
+
+      default:
+      Serial.println("Setting reference...");
+        ref = (chr-'0')*110;
+        Serial.println(ref, DEC);
+        break;
+    }
+  }
+
+ /* t=millis();
+  if((t-tOld)>stepTime)
+  {
+    ref = refList[ct];
+
+    ct++;
+    if(ct>=sizeof(refList)/sizeof(int))
+    {
+      ct=0;
+    }
+    tOld = t;
+  }*/
+}
+
+/** @brief Interrupt callback at every pulse from the alternator 
+
+Approximately 2 pulses every 10 ms.
+*/
+void wInterrupt()
+{
+wCounter++;
+}
+
